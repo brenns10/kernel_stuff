4700b31ab433dbc7db1889f8d4fd8add58bf5a19
Stephen Brennan <stephen.s.brennan@oracle.com>
2024-05-13 16:28:07 -0700

    negdentcreate: several new capabilities
    I should break this down into separate changes, but I won't.
    
    1. Add "operations" that get run against each filename. The --op/-o
    argument defaults to "stat", but you can use "open" to open in read
    mode, "create" to create a file, and "unlink" to remove it.
    
    2. Add "--loop" option that will allow the workers to loop running the
    same operation over and over again, until an interrupt signal is
    received.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- negdentcreate/negdentcreate.c
+++ negdentcreate/negdentcreate.c
@@ -31,25 +31,90 @@
 #include <pthread.h>
 #include <errno.h>
 #include <time.h>
+#include <signal.h>
+#include <stdbool.h>
+
+#define nelem(arr) (sizeof(arr) / sizeof(arr[0]))
+
+bool loop;
+bool exiting;
+
+typedef int (*work_op_t)(int, const char *);
 
 struct work {
 	struct work *next;
 	const char *path;
 	const char *pfx;
+	work_op_t op;
 	unsigned long start;
 	unsigned long stop;
 	unsigned long cur;
+	unsigned long cnt;
 	pthread_t thread;
 	int error;
 };
 
-void *stat_worker(void *varg)
+static int do_stat(int dirfd, const char *filename)
+{
+	struct stat statbuf;
+	if (fstatat(dirfd, filename, &statbuf, 0) == -1 && errno != ENOENT) {
+		perror("fstatat");
+		return -1;
+	}
+	return 0;
+}
+
+static int do_open(int dirfd, const char *filename)
+{
+	int fd = openat(dirfd, filename, O_RDONLY);
+	if (fd < 0) {
+		perror("openat");
+		return -1;
+	}
+	if (close(fd) != 0) {
+		perror("close");
+		return -1;
+	}
+	return 0;
+}
+
+static int do_create(int dirfd, const char *filename)
+{
+	int fd = openat(dirfd, filename, O_WRONLY | O_CREAT, 0644);
+	if (fd < 0) {
+		perror("openat");
+		return -1;
+	}
+	if (close(fd) != 0) {
+		perror("close");
+		return -1;
+	}
+	return 0;
+}
+
+static int do_unlink(int dirfd, const char *filename)
+{
+	int rv = unlinkat(dirfd, filename, 0);
+	if (rv < 0) {
+		perror("openat");
+		return -1;
+	}
+	return 0;
+}
+
+struct { char *name; work_op_t op; } OPERATIONS[] = {
+	{ "stat", do_stat },
+	{ "open", do_open },
+	{ "create", do_create },
+	{ "unlink", do_unlink },
+};
+
+static void *stat_worker(void *varg)
 {
 	struct work *arg = (struct work *)varg;
 	struct stat statbuf;
 	char filename[64];
 	int dirfd;
-
 
 	dirfd = open(arg->path, O_DIRECTORY | O_PATH);
 	if (dirfd == -1) {
@@ -58,15 +123,18 @@ void *stat_worker(void *varg)
 		return NULL;
 	}
 
-	for (arg->cur = arg->start; arg->cur < arg->stop; arg->cur++) {
-		snprintf(filename, sizeof(filename), "%s%010lu", arg->pfx, arg->cur);
-		if (fstatat(dirfd, filename, &statbuf, 0) == -1 && errno != ENOENT) {
-			perror("fstatat");
-			close(dirfd);
-			arg->error = 1;
-			return NULL;
+	do {
+		for (arg->cur = arg->start; arg->cur < arg->stop && !exiting; arg->cur++) {
+			snprintf(filename, sizeof(filename), "%s%010lu", arg->pfx, arg->cur);
+			if (arg->op(dirfd, filename) == -1) {
+				close(dirfd);
+				arg->error = 1;
+				return NULL;
+			}
 		}
-	}
+		if (loop)
+			arg->cnt++;
+	} while (loop && !exiting);
 	close(dirfd);
 	return NULL;
 }
@@ -86,8 +154,15 @@ void help(void)
 	printf("  -p, --path <PATH>  create negative dentries in PATH\n");
 	printf("  -P, --pfx  <STR>   name dentries with STR + 10-digit number\n");
 	printf("                     (default is \"file-\")\n");
+	printf("  -o, --op <STR>     operation (choices: stat, open, create, unlink)\n");
+	printf("  -l, --loop         loop continuously, re-accessing\n");
 	printf("  -h, --help         show this message and exit.\n");
 	exit(EXIT_SUCCESS);
+}
+
+void interrupt(int signal)
+{
+	exiting = true;
 }
 
 int main(int argc, char **argv)
@@ -98,18 +173,45 @@ int main(int argc, char **argv)
 	char *path = get_current_dir_name();
 	char *pfx = "file-";
 	int opt;
-	const char *shopt = "t:c:p:P:h";
+	const char *shopt = "t:c:p:P:o:hl";
+	sigset_t set;
+	struct sigaction sa = {0};
+	work_op_t op = &do_stat;
+
+	/* Block SIGINT so that threads inherit this. */
+	sigemptyset(&set);
+	sigaddset(&set, SIGINT);
+	err = pthread_sigmask(SIG_BLOCK, &set, NULL);
+	if (err != 0) {
+		errno = err;
+		perror("pthread_sigmask");
+		exit(1);
+	}
+
+	/* Setup our SIGINT handler for later. */
+	sa.sa_handler = interrupt;
+	err = sigaction(SIGINT, &sa, NULL);
+	if (err != 0) {
+		perror("sigaction");
+		exit(1);
+	}
+
 	static struct option lopt[] = {
 		{"--threads", required_argument, NULL, 't'},
 		{"--count",   required_argument, NULL, 'c'},
 		{"--path",    required_argument, NULL, 'p'},
 		{"--help",    no_argument,       NULL, 'h'},
+		{"--loop",    no_argument,       NULL, 'l'},
+		{"--op",      required_argument, NULL, 'o'},
 		{"--prefix",  required_argument, NULL, 'P'},
 	};
 	while ((opt = getopt_long(argc, argv, shopt, lopt, NULL)) != -1) {
 		switch (opt) {
 			case 'h':
 				help();
+				break;
+			case 'l':
+				loop = true;
 				break;
 			case 't':
 				nthread = atoi(optarg);
@@ -120,6 +222,18 @@ int main(int argc, char **argv)
 			case 'p':
 				free(path);
 				path = strdup(optarg);
+				break;
+			case 'o':
+				for (i = 0; i < nelem(OPERATIONS); i++) {
+					if (strcmp(OPERATIONS[i].name, optarg) == 0) {
+						op = OPERATIONS[i].op;
+						break;
+					}
+				}
+				if (i >= nelem(OPERATIONS)) {
+					fprintf(stderr, "--op %s : operation unknown\n", optarg);
+					exit(EXIT_FAILURE);
+				}
 				break;
 			case 'P':
 				pfx = optarg;
@@ -143,10 +257,15 @@ int main(int argc, char **argv)
 		cur->path = path;
 		cur->start = i * increment;
 		cur->stop = cur->start + increment;
+		cur->cnt = 0;
 		cur->cur = cur->start;
 		cur->pfx = pfx;
+		cur->op = op;
 		pthread_create(&cur->thread, NULL, stat_worker, cur);
 	}
+
+	/* Unblock SIGINT now that threads are created */
+	err = pthread_sigmask(SIG_UNBLOCK, &set, NULL);
 
 	do {
 		struct timespec tv;
@@ -154,19 +273,23 @@ int main(int argc, char **argv)
 		err = 0;
 		for (cur = first; cur; cur = cur->next) {
 			progress += cur->cur - cur->start;
+			progress += cur->cnt * (cur->stop - cur->start);
 			err += cur->error;
 		}
 		printf("progress: %10lu/%10lu\n", progress, count);
-		tv.tv_nsec = 10 * 1000 * 1000; /* 10ms */
+		tv.tv_nsec = 100 * 1000 * 1000; /* 100ms */
 		tv.tv_sec = 0;
 		nanosleep(&tv, NULL);
-	} while (progress < count && err == 0);
+	} while ((progress < count || loop) && err == 0 && !exiting);
 
 	if (err) {
 		fprintf(stderr, "error detected! canceling threads\n");
 		for (cur = first; cur; cur = cur->next)
 			if (!cur->error)
 				pthread_cancel(cur->thread);
+		err = EXIT_FAILURE;
+	} else if (exiting) {
+		fprintf(stderr, "interrupted! waiting on threads\n");
 		err = EXIT_FAILURE;
 	} else {
 		fprintf(stderr, "done! waiting on threads\n");
